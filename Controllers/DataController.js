13375e168c2fedfaa0ae555c81dc589d49bbc0d7
Santiago Guerra <santiago.guerra@almundo.com>
2019-09-09 08:06:50 -0300

    completing validate on dataController

--- Controllers/DataController.js
+++ Controllers/DataController.js
@@ -1,27 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-const SORT_VALIDATE = ["asc", "desc"];
-const FIELD_VALIDATE = ["name", "segment1", "segment2", "segment3", "segment4", "plataformId", "clientId"];
+const SORTS = ["asc", "desc"];
+const FIELDS = ["name", "segment1", "segment2", "segment3", "segment4", "plataformId", "clientId"];
 
 router.get('/data', (req, res) => {
 
     const query = req.query;
     const { sort, sortField } = query;
-    const fields = JSON.parse(query.fields);
-    const limit = Number.parseInt(query.limit || 10);
+    const fields = query.fields ? JSON.parse(query.fields) : FIELDS;
+    const limit = query.limit || "10";
 
-    const isInvalid = 
-        (!SORT_VALIDATE.includes(sort)) ||
-        (!FIELD_VALIDATE.includes(sortField)) ||
-        (!FIELD_VALIDATE.some(field => !fields.includes(field))) //||
-        //!Number.isNan(limit);
-
+    const isInvalid =
+        isInvalidParamSort(sort) ||
+        isInvalidParamSortField(sortField) ||
+        isInvalidParamFields(fields) ||
+        isInvalidParamLimit(limit);
 
     if (isInvalid) {
 
         res.status(400).json({
-            response: "Is invalid"
+            response: "Bad request"
         })
 
     } else {
@@ -37,5 +36,21 @@ router.get('/data', (req, res) => {
 
     
 });
+
+const isInvalidParamSort = sort => !SORTS.includes(sort);
+
+const isInvalidParamSortField = sortField => !FIELDS.includes(sortField);
+
+const isInvalidParamFields = fields => !fields.length > 0 || !fields.every(field => FIELDS.includes(field));
+
+const isInvalidParamLimit = limit => !isInteger(limit);
+
+const isInteger = (stringNumber) => {
+
+    const number = Math.floor(Number(stringNumber));
+
+    return number !== Infinity && String(number) === stringNumber && number > 0;
+
+}
 
 module.exports = router;
