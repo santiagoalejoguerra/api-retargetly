d515c6d5c9ac928b354a727a3cde53c1146106a4
Santiago Guerra <santiago.guerra@almundo.com>
2019-09-10 08:40:36 -0300

    adding login implementation

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,12 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-    
-    const body = req.body;
-    
-    res.json({ response: body});
+const jwt = require('jsonwebtoken')
+const randtoken = require('rand-token')
 
+const tokens = {};
+const SECRET = "SECRETO_PARA_ENCRIPTACION";
+
+router.post('/login', (req, res, next) => {
+    
+    const {username, password} = req.body;
+
+    const user = {
+        'username': username
+    }
+
+      console.log(user);
+
+    const token = jwt.sign(user, SECRET, { expiresIn: 15 });
+
+    tokens[token] = user;
+
+    res.json({
+        token: token,
+    });
+
+});
+
+router.post('/token', (req, res, next) => {
+
+    const { token, username } = req.headers;
+
+    if ((token in tokens) && (tokens[token].username == username)) {
+
+        const user = {
+            'username': username
+        }
+
+        const token = jwt.sign(user, SECRET, { expiresIn: 15 })
+
+        res.json({
+            token: token
+        })
+
+    } else {
+
+        res.status(401).json("Unauthorized");
+
+    }
 
 });
 
